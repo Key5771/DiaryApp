4176666c284b3423a2e54a93e5f37853bb984234
key <ksj57715@gmail.com>
2019-07-22 17:53:42 +0900

    android calendar library

--- Android/My App/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Android/My App/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,14 +3,11 @@ package com.example.myapplication;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.marcohc.robotocalendar.RobotoCalendarView;
 
-import java.util.Calendar;
 import java.util.Date;
-import java.util.Random;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
@@ -24,29 +21,6 @@ public class MainActivity extends AppCompatActivity implements RobotoCalendarVie
         setContentView(R.layout.activity_main);
 
         robotoCalendarView = findViewById(R.id.robotoCalendarPicker);
-        Button markDayButton = findViewById(R.id.markDayButton);
-        Button clearSelectedDayButton = findViewById(R.id.clearSelectedDayButton);
-
-        markDayButton.setOnClickListener(view -> {
-            Calendar calendar = Calendar.getInstance();
-            Random random = new Random(System.currentTimeMillis());
-            int style = random.nextInt(2);
-            int daySelected = random.nextInt(calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
-            calendar.set(Calendar.DAY_OF_MONTH, daySelected);
-
-            switch (style) {
-                case 0:
-                    robotoCalendarView.markCircleImage1(calendar.getTime());
-                    break;
-                case 1:
-                    robotoCalendarView.markCircleImage2(calendar.getTime());
-                    break;
-                default:
-                    break;
-            }
-        });
-
-        clearSelectedDayButton.setOnClickListener(v -> robotoCalendarView.clearSelectedDay());
 
         robotoCalendarView.setRobotoCalendarListener(this);
 
