86c13f54cd4b5d8f558e44b23d7d19c877f690a7
Dohyun Lee <xxrcy32@gmail.com>
2019-08-14 14:55:07 +0900

    DB 삭제/ 추가 기능!

--- Android/My App/app/src/main/java/com/example/myapplication/Activity/MainActivity.java
+++ Android/My App/app/src/main/java/com/example/myapplication/Activity/MainActivity.java
@@ -1,41 +1,43 @@
 package com.example.myapplication.Activity;
 
-import android.content.ContentValues;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.myapplication.Adapter.DiaryAdapter;
 import com.example.myapplication.Database.TaskContract;
 import com.example.myapplication.Database.TaskDbHelper;
 import com.example.myapplication.Fragment.CalendarFragment;
 import com.example.myapplication.Fragment.DiaryFragment;
 import com.example.myapplication.Fragment.FeelingFragment;
 import com.example.myapplication.Fragment.SettingFragment;
+import com.example.myapplication.Model.DiaryContent;
 import com.example.myapplication.R;
 import com.marcohc.robotocalendar.RobotoCalendarView;
 
 
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
-public class MainActivity extends AppCompatActivity implements RobotoCalendarView.RobotoCalendarListener {
+public class MainActivity extends AppCompatActivity  {
 
     private RobotoCalendarView robotoCalendarView;
     private FragmentManager fragmentManager = getSupportFragmentManager();
@@ -43,9 +45,10 @@ public class MainActivity extends AppCompatActivity implements RobotoCalendarVie
     private FeelingFragment feelingFragment = new FeelingFragment();
     private SettingFragment settingFragment = new SettingFragment();
     private CalendarFragment calendarFragment = new CalendarFragment();
-    private Fragment fragment = new Fragment();
     private TaskDbHelper mHelper;
     private ArrayAdapter<String> DiaryFragment;
+    private FloatingActionButton fab;
+    private ListView listOfDiary;
 
     private static final String TAG = "MainActivity";
 //    private CalendarView calendarView = new CalendarView();
@@ -55,32 +58,25 @@ public class MainActivity extends AppCompatActivity implements RobotoCalendarVie
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 달력 뷰
-//        robotoCalendarView = findViewById(R.id.robotoCalendarPicker);
-//        robotoCalendarView.setRobotoCalendarListener(this);
-//        robotoCalendarView.setShortWeekDays(false);
-//        robotoCalendarView.showDateTitle(true);
-//        robotoCalendarView.setDate(new Date());
-
-
         //DB 저장
         mHelper = new TaskDbHelper(this);
         SQLiteDatabase db = mHelper.getReadableDatabase();
-    Cursor cursor = db.query(TaskContract.TaskEntry.TABLE,
-            new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.COL_TASK_TITLE},
-            null, null, null, null, null);
-    while(cursor.moveToNext()){
-        int idx = cursor.getColumnIndex(TaskContract.TaskEntry.COL_TASK_TITLE);
-        Log.d(TAG, "Task: " + cursor.getString(idx));
-    }
-    cursor.close();
+        Cursor cursor = db.query(TaskContract.TaskEntry.TABLE,
+                new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.COL_TASK_TITLE},
+                null, null, null, null, null);
+        while(cursor.moveToNext()){
+            int idx = cursor.getColumnIndex(TaskContract.TaskEntry.COL_TASK_TITLE);
+            Log.d(TAG, "Task: " + cursor.getString(idx));
+        }
+        cursor.close();
     db.close();
 
 
     //하단 메뉴 뷰
-    BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+    BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigation);
     FragmentTransaction transaction = fragmentManager.beginTransaction();
-    transaction.replace(R.id.frame_layout, fragment).commitAllowingStateLoss();
+    transaction.replace(R.id.frame_layout, calendarFragment).commitAllowingStateLoss();
+
 
     bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -89,23 +85,21 @@ public class MainActivity extends AppCompatActivity implements RobotoCalendarVie
             FragmentTransaction transaction = fragmentManager.beginTransaction();
             switch (menuItem.getItemId()){
                 case R.id.calendar:{
-                    transaction.replace(R.id.frame_layout, calendarFragment).commitAllowingStateLoss();
+                    transaction.replace(R.id.frame_layout, calendarFragment).commit();
                     break;
                 }
                 case R.id.diary:{
-                    transaction.replace(R.id.frame_layout, diaryFragment).commitAllowingStateLoss();
+                    transaction.replace(R.id.frame_layout, diaryFragment).commit();
                     break;
                 }
                 case R.id.mood:{
-                    transaction.replace(R.id.frame_layout, feelingFragment).commitAllowingStateLoss();
+                    transaction.replace(R.id.frame_layout, feelingFragment).commit();
                     break;
                 }
-
                 case R.id.setting:{
-                    transaction.replace(R.id.frame_layout, settingFragment).commitAllowingStateLoss();
+                    transaction.replace(R.id.frame_layout, settingFragment).commit();
                     break;
                 }
-
             }
             return true;
         }
@@ -114,82 +108,52 @@ public class MainActivity extends AppCompatActivity implements RobotoCalendarVie
     }
 
     // 일기 어댑터
-//    private ListView mDiaryListView;
+    List<DiaryContent> diaryContentList = new LinkedList<>();
+    private ListView mDiaryListView;
 
-//    private void upDateUI(){
-//        ArrayList<String> DiaryList = new ArrayList<>();
-//        SQLiteDatabase db = mHelper.getReadableDatabase();
-//        Cursor cursor = db.query(TaskContract.TaskEntry.TABLE,
-//                new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.COL_TASK_TITLE},
-//                null, null, null, null,null);
-//        while(cursor.moveToNext()){
-//            int idx = cursor.getColumnIndex(TaskContract.TaskEntry.COL_TASK_TITLE);
-//            DiaryList.add(cursor.getString(idx));
-//        }
 
-//        if(DiaryFragment == null) {
-//            DiaryFragment = new ArrayAdapter<>(this, R.layout.item_diary, R.id.diary_title, DiaryList);
-//            mDiaryListView.setAdapter(DiaryFragment);
-//        } else{
-//            DiaryFragment.clear();
-//            DiaryFragment.addAll(DiaryList);
-//            DiaryFragment.notifyDataSetChanged();
-//        }
+    private void upDateUI(){
+        ArrayList<String> DiaryList = new ArrayList<>();
+        SQLiteDatabase db = mHelper.getReadableDatabase();
+        Cursor cursor = db.query(TaskContract.TaskEntry.TABLE,
+                new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.COL_TASK_TITLE},
+                null, null, null, null,null);
+        while(cursor.moveToNext()){
+            int idx = cursor.getColumnIndex(TaskContract.TaskEntry.COL_TASK_TITLE);
+            DiaryList.add(cursor.getString(idx));
+        }
 
-//        cursor.close();
-//        db.close();
-//    }
+        mDiaryListView = (ListView) findViewById(R.id.diary_list);
+
+        if(DiaryFragment == null) {
+            DiaryFragment = new ArrayAdapter<>(this, R.layout.diary_list_item, R.id.diary_item_title, DiaryList);
+            mDiaryListView.setAdapter(DiaryFragment);
+        } else{
+            DiaryFragment.clear();
+            DiaryFragment.addAll(DiaryList);
+            DiaryFragment.notifyDataSetChanged();
+        }
+
+        cursor.close();
+        db.close();
+    }
+
+    //삭제 기능
+    public void deleteDiary(View view){
+        View parent = (View) view.getParent();
+        TextView diaryTextView = (TextView) parent.findViewById(R.id.diary_item_title);
+        String diary = String.valueOf(diaryTextView.getText());
+        SQLiteDatabase db = mHelper.getWritableDatabase();
+        db.delete(TaskContract.TaskEntry.TABLE, TaskContract.TaskEntry.COL_TASK_TITLE + " = ?", new String[]{diary});
+        db.close();
+        upDateUI();
+
+    }
 
 
     @Override
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
-
-    private void showAddItemDialog(Context c){
-        final EditText taskEditText = new EditText(c);
-        AlertDialog dialog = new AlertDialog.Builder(c)
-                .setTitle("Add a new diary")
-                .setMessage("What did you do today?")
-                .setView(taskEditText)
-                .setPositiveButton("ADD", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        String task = String.valueOf(taskEditText.getText());
-                        SQLiteDatabase db = mHelper.getWritableDatabase();
-                        ContentValues values = new ContentValues();
-                        values.put(TaskContract.TaskEntry.COL_TASK_TITLE, task);
-                        db.insertWithOnConflict(TaskContract.TaskEntry.TABLE, null, values, SQLiteDatabase.CONFLICT_REPLACE);
-                        db.close();
-                    }
-                })
-                .setNegativeButton("Cancel", null)
-                .create();
-        dialog.show();
-    }
-
-
-    @Override
-    public void onDayClick(Date date) {
-        Toast.makeText(this, "onDayClick : " + date, Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onDayLongClick(Date date) {
-//        Toast.makeText(this, "onDayLongClick: " + date, Toast.LENGTH_SHORT).show();
-        showAddItemDialog(MainActivity.this);
-    }
-
-    @Override
-    public void onRightButtonClick() {
-        Toast.makeText(this, "onRightButtonClick!", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onLeftButtonClick() {
-        Toast.makeText(this, "onLeftButtonClick!", Toast.LENGTH_SHORT).show();
-    }
-
-
 
 }
--- Android/My App/app/src/main/java/com/example/myapplication/Fragment/CalendarFragment.java
+++ Android/My App/app/src/main/java/com/example/myapplication/Fragment/CalendarFragment.java
@@ -1,6 +1,11 @@
 package com.example.myapplication.Fragment;
 
+import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
@@ -10,23 +15,39 @@ import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.graphics.drawable.DrawableCompat;
+import android.support.v7.app.AlertDialog;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.myapplication.Activity.MainActivity;
 import com.example.myapplication.CalendarView;
+import com.example.myapplication.Database.TaskContract;
+import com.example.myapplication.Database.TaskDbHelper;
 import com.example.myapplication.R;
+import com.marcohc.robotocalendar.RobotoCalendarView;
 
 import java.text.DateFormatSymbols;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
-public class CalendarFragment extends Fragment {
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+public class CalendarFragment extends Fragment implements RobotoCalendarView.RobotoCalendarListener {
+
+    private RobotoCalendarView robotoCalendarView;
+    private TaskDbHelper mHelper;
+    Activity root = getActivity();
+
+
     @Override
     @Nullable
 
@@ -34,7 +55,61 @@ public class CalendarFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.activity_calendar_fragment,container,false);
 
+        mHelper = new TaskDbHelper(getContext());
+
+        robotoCalendarView = (RobotoCalendarView) view.findViewById(R.id.robotoCalendarPicker);
+        robotoCalendarView.setRobotoCalendarListener(this);
+        robotoCalendarView.setShortWeekDays(false);
+        robotoCalendarView.showDateTitle(true);
+        robotoCalendarView.setDate(new Date());
+
         return view;
+    }
+
+
+    private void showAddItemDialog(Context c){
+        final EditText taskEditText = new EditText(c);
+        AlertDialog dialog = new AlertDialog.Builder(c)
+                .setTitle("일기 쓰기")
+                .setMessage("오늘 뭐했나요?")
+                .setView(taskEditText)
+                .setPositiveButton("추가", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String task = String.valueOf(taskEditText.getText());
+                        SQLiteDatabase db = mHelper.getWritableDatabase();
+                        ContentValues values = new ContentValues();
+                        values.put(TaskContract.TaskEntry.COL_TASK_TITLE, task);
+                        db.insertWithOnConflict(TaskContract.TaskEntry.TABLE, null, values, SQLiteDatabase.CONFLICT_REPLACE);
+//                        db.insert(TaskContract.TaskEntry.TABLE, null, values);
+                        db.close();
+                    }
+                })
+                .setNegativeButton("취소", null)
+                .create();
+        dialog.show();
+    }
+
+
+    @Override
+    public void onDayClick(Date date) {
+        Toast.makeText(getActivity(), "onDayClick : " + date, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDayLongClick(Date date) {
+//        Toast.makeText(getActivity(), "onDayLongClick: " + date, Toast.LENGTH_SHORT).show();
+        showAddItemDialog(getContext());
+    }
+
+    @Override
+    public void onRightButtonClick() {
+        Toast.makeText(getActivity(), "onRightButtonClick!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onLeftButtonClick() {
+        Toast.makeText(getActivity(), "onLeftButtonClick!", Toast.LENGTH_SHORT).show();
     }
 
 }
