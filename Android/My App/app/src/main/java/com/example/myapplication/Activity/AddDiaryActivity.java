941fd0240cea92facd9184ad78a846e8d55ab78a
Dohyun Lee <xxrcy32@gmail.com>
2019-08-21 13:35:55 +0900

    firebase 연동

--- Android/My App/app/src/main/java/com/example/myapplication/Activity/AddDiaryActivity.java
+++ Android/My App/app/src/main/java/com/example/myapplication/Activity/AddDiaryActivity.java
@@ -4,7 +4,10 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,10 +15,22 @@ import android.widget.EditText;
 import com.example.myapplication.Database.TaskContract;
 import com.example.myapplication.Database.TaskDbHelper;
 import com.example.myapplication.Fragment.CalendarFragment;
+import com.example.myapplication.Model.DiaryContent;
 import com.example.myapplication.R;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AddDiaryActivity extends AppCompatActivity {
     private TaskDbHelper mHelper;
+
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference titleRef = databaseReference.child("title");
+    DatabaseReference contentRef = databaseReference.child("content");
+
 
     Button save_btn;
     EditText title, content;
@@ -24,27 +39,26 @@ public class AddDiaryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_diary);
-        mHelper = new TaskDbHelper(this);
+//        mHelper = new TaskDbHelper(this);
 
         init();
 
-        save_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String get_title = String.valueOf(title.getText());
-                String get_content = String.valueOf(title.getText());
-                SQLiteDatabase db = mHelper.getWritableDatabase();
-                ContentValues values = new ContentValues();
-                values.put(TaskContract.TaskEntry.COL_TASK_TITLE, get_title);
-                values.put(TaskContract.TaskEntry.COL_TASK_CONTENT, get_content);
-                db.insertWithOnConflict(TaskContract.TaskEntry.TABLE, null, values, SQLiteDatabase.CONFLICT_REPLACE);
-                db.close();
-
-//                Intent backtoActivity = new Intent(AddDiaryActivity.this, CalendarFragment.class);
-//                startActivity(backtoActivity);
-                finish();
-            }
-        });
+//        save_btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String get_title = String.valueOf(title.getText());
+//                String get_content = String.valueOf(title.getText());
+//                SQLiteDatabase db = mHelper.getWritableDatabase();
+//                ContentValues values = new ContentValues();
+//                values.put(TaskContract.TaskEntry.COL_TASK_TITLE, get_title);
+//                values.put(TaskContract.TaskEntry.COL_TASK_CONTENT, get_content);
+//                db.insertWithOnConflict(TaskContract.TaskEntry.TABLE, null, values, SQLiteDatabase.CONFLICT_REPLACE);
+//                db.close();
+//
+//
+//                finish();
+//            }
+//        });
     }
 
     public void init() {
@@ -53,5 +67,19 @@ public class AddDiaryActivity extends AppCompatActivity {
         save_btn = (Button) findViewById(R.id.save_button);
     }
 
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        save_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                DiaryContent diaryContent = new DiaryContent(title.getText().toString(),content.getText().toString());
+                databaseReference.child("message").push().setValue(diaryContent);
+
+                finish();
+            }
+        });
+    }
 
 }
--- Android/My App/app/src/main/java/com/example/myapplication/Fragment/DiaryFragment.java
+++ Android/My App/app/src/main/java/com/example/myapplication/Fragment/DiaryFragment.java
@@ -14,6 +14,7 @@ import android.view.ViewGroup;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -23,6 +24,12 @@ import com.example.myapplication.Database.TaskContract;
 import com.example.myapplication.Database.TaskDbHelper;
 import com.example.myapplication.Model.DiaryContent;
 import com.example.myapplication.R;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -31,6 +38,9 @@ import java.util.List;
 public class DiaryFragment extends Fragment {
     private ListView mDiaryListView;
     private DiaryAdapter adapter;
+
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference titleRef = databaseReference.child("title");
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -46,39 +56,53 @@ public class DiaryFragment extends Fragment {
 //        mDiaryListView = (ListView) getView().findViewById(R.id.list_diary);
 
         List<DiaryContent> diaryContentList = new LinkedList<>();
-
-        TaskDbHelper taskDbHelper = new TaskDbHelper(getContext());
-        SQLiteDatabase sqLiteDatabase = taskDbHelper.getWritableDatabase();
-        Cursor cursor = sqLiteDatabase.query(TaskContract.TaskEntry.TABLE, new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.COL_TASK_TITLE, TaskContract.TaskEntry.COL_TASK_CONTENT},
-                null, null, null, null, null);
-
-        while (cursor.moveToNext()) {
-            DiaryContent content = new DiaryContent();
-            content.setTitle(cursor.getString(1));
-            content.setContent(cursor.getString(2));
-            diaryContentList.add(content);
-        }
-
+//
+//        TaskDbHelper taskDbHelper = new TaskDbHelper(getContext());
+//        SQLiteDatabase sqLiteDatabase = taskDbHelper.getWritableDatabase();
+//        Cursor cursor = sqLiteDatabase.query(TaskContract.TaskEntry.TABLE, new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.COL_TASK_TITLE, TaskContract.TaskEntry.COL_TASK_CONTENT},
+//                null, null, null, null, null);
+//
+//        while (cursor.moveToNext()) {
+//            DiaryContent content = new DiaryContent();
+//            content.setTitle(cursor.getString(1));
+//            content.setContent(cursor.getString(2));
+//            diaryContentList.add(content);
+//        }
+//
         mDiaryListView = view.findViewById(R.id.diary_list);
         mDiaryListView.setAdapter(new DiaryAdapter(diaryContentList));
+//
+//        mDiaryListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//
+//                int textRes = ((DiaryContent)adapter.getItem(position)).getID();
+//
+//                Intent intent = new Intent(getActivity(), DetailActivity.class);
+//                intent.putExtra("textRes",textRes);
+//                intent.putExtra("title", TaskContract.TaskEntry.COL_TASK_TITLE);
+//                intent.putExtra("content", TaskContract.TaskEntry.COL_TASK_CONTENT);
+//                startActivity(intent);
+//
+//
+//            }
+//        });
 
-        mDiaryListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+        database.child("message").addValueEventListener(new ChildEventListener(){
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                int textRes = ((DiaryContent)adapter.getItem(position)).getID();
-
-                Intent intent = new Intent(getActivity(), DetailActivity.class);
-                intent.putExtra("textRes",textRes);
-                intent.putExtra("title", TaskContract.TaskEntry.COL_TASK_TITLE);
-                intent.putExtra("content", TaskContract.TaskEntry.COL_TASK_CONTENT);
-                startActivity(intent);
+            public void onChildAdded(DataSnapshot dataSnapshot, String s){
+                DiaryContent diaryContent = dataSnapshot.getValue(DiaryContent.class);
+                adapter.add()
             }
-        });
+        })
 
         return view;
     }
-}
+
+
+    }
+
 
 
 
