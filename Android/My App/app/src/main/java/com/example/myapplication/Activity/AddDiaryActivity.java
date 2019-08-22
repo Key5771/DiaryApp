d03c6ce2d1c7ec58432daf9e26043980026fde17
Lee Do Hyun <xxrcy32@gmail.com>
2019-08-22 11:34:58 +0900

    firebase

--- Android/My App/app/src/main/java/com/example/myapplication/Activity/AddDiaryActivity.java
+++ Android/My App/app/src/main/java/com/example/myapplication/Activity/AddDiaryActivity.java
@@ -4,10 +4,11 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.Database.TaskContract;
 import com.example.myapplication.Database.TaskDbHelper;
