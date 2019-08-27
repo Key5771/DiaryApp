14f0d1fcbcd437994617429e5b38ce3782eb04da
key <ksj57715@gmail.com>
2019-08-27 17:30:45 +0900

    ios 프로젝트 추가

---
+++ ios/Calendary/Calendary/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Calendary
+//
+//  Created by 김기현 on 23/08/2019.
+//  Copyright © 2019 김기현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var editButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        editButton.layer.masksToBounds = false
+        editButton.layer.shadowColor = UIColor.blue.cgColor
+        editButton.layer.shadowRadius = 10
+        editButton.layer.cornerRadius = 35
+    }
+
+
+}
+
